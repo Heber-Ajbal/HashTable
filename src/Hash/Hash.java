f89fb078e22c258d804095636e7917e946aee223
Heber <heberajbal2003@gmail.com>
2024-05-30 18:47:26 -0600

    Remove
    remove code

--- src/Hash/Hash.java
+++ src/Hash/Hash.java
@@ -28,10 +28,6 @@ import javafx.stage.Stage;
  */
 public class Hash extends Application {
     
-    connection con = new connection();
-    Connection connect;
-    Statement st;
-    ResultSet rs;
         
     @Override
     public void start(Stage primaryStage) {
@@ -80,64 +76,7 @@ public class Hash extends Application {
         
     }*/
     
-      private void query() {
-        BufferedWriter writer = null;
-        try {
-            // Configura la conexión a la base de datos y ejecuta la consulta
-            String sql = "SELECT c.id as IDC, s.id as IDS, s.iso, c.name FROM states s JOIN cities c ON s.id = c.id_state";
-            connect = con.gertConnection(); // Asegúrate de que con.gertConnection() está bien definido
-            st = connect.createStatement();
-            rs = st.executeQuery(sql);
-
-            // Inicializa el BufferedWriter para escribir en el archivo
-            writer = new BufferedWriter(new FileWriter("D:\\output.txt"));
-
-            // Itera a través de los resultados de la consulta
-            while (rs.next()) {
-                String isoNuevo; 
-                String iso = rs.getString("iso");
-                String stateName = rs.getString("name");                
-                Integer id = Integer.parseInt(rs.getString("IDC"));                                
-                Integer idC = Integer.parseInt(rs.getString("IDS")); 
-                String ultimasDosLetras = iso.substring(iso.length() - 2);
-                if (stateName.length() >= 2) {
-                  isoNuevo = ultimasDosLetras + stateName.substring(0,2) + stateName.substring(stateName.length() - 1); // Combinar iso y las dos primeras letras del nombre del estado
-                }else{
-                  isoNuevo = ultimasDosLetras + stateName.substring(0,1) + stateName.substring(stateName.length() - 1); // Combinar iso y las dos primeras letras del nombre del estado
-                }
-               
-
-                // Escribe el resultado en el archivo en lugar de imprimir en consola
-                writer.write("INSERT INTO cities (id, id_state, name, iso) VALUES ('" + id + "', '" + idC + "', '" + stateName + "', '" + isoNuevo + "');");
-                writer.newLine(); // Escribe una nueva línea en el archivo
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Cierra el BufferedWriter y los recursos de la base de datos
-            try {
-                if (writer != null) {
-                    writer.close();
-                }
-                if (rs != null) {
-                    rs.close();
-                }
-                if (st != null) {
-                    st.close();
-                }
-                if (connect != null) {
-                    connect.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
     
-        
         
 }
     
